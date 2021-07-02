0b8d068984c2c26f645112b2604c77245df7fcbf
rubenbimmel <ruben.bimmel@gmail.com>
2021-07-02 11:14:36 +0200

    remove obsolete prop decorator

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Alpine from 'alpinejs';
 import AlpineComponent from './alpineComponent';
 import Component from './decorators/component';
-import Prop from './decorators/prop';
 import Ref from './decorators/ref';
 import Watch from './decorators/watch';
 
@@ -9,7 +8,6 @@ export {
     Alpine,
     AlpineComponent,
     Component,
-    Prop,
     Ref,
     Watch
 }
